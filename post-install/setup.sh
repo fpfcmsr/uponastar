cb7a9b7e9fbca75452305cf00309dded0f35a609
fpfcmsr <fpf@andrew.cmu.edu>
2025-04-16 17:37:01 -0400

    Update setup.sh

--- post-install/setup.sh
+++ post-install/setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 ujust dx-group
+
+mkdir /home/$user/Documents/SYSTEM-EDITS
+git clone https://github.com/cfgnunes/nautilus-scripts.git /home/$user/Documents/SYSTEM-EDITS
+cp nautilus-install.sh /home/$user/Documents/SYSTEM-EDITS/nautilus-scripts
+bash nautilus-install.sh
+
