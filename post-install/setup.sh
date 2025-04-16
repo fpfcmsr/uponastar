42716af925288b3bbb06f69ad621034f5bfcb75d
fpfcmsr <fpf@andrew.cmu.edu>
2025-04-16 17:39:08 -0400

    Update setup.sh

--- post-install/setup.sh
+++ post-install/setup.sh
@@ -6,3 +6,15 @@ git clone https://github.com/cfgnunes/nautilus-scripts.git /home/$user/Documents
 cp nautilus-install.sh /home/$user/Documents/SYSTEM-EDITS/nautilus-scripts
 bash nautilus-install.sh
 
+# edit gpu switching config to enable seamless hotplug
+rm /etc/supergfxd.conf
+touch /etc/supergfxd.conf
+echo '{
+“mode”: “Hybrid”,
+“vfio_enable”: true,
+“vfio_save”: false,
+“always_reboot”: false,
+“no_logind”: true,
+“logout_timeout_s”: 60,
+“hotplug_type”: “Asus”
+}' >> /etc/supergfxd.conf
