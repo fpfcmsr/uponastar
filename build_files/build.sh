c02ff720fd75b7421be0e61cc3ff895fd9bea9b8
fpfcmsr <fpf@andrew.cmu.edu>
2025-04-16 02:21:18 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -56,38 +56,10 @@ ln -s /var/opt /opt
 cd /tmp
 git clone https://github.com/cfgnunes/nautilus-scripts.git
 cd nautilus-scripts
-cp /ctx/script.exp /tmp/nautilus-scripts/script.exp
-#touch script.exp
-#echo '#!/usr/bin/expect -f
-#
-#set timeout -1
-#spawn bash install.sh
-#match_max 100000
-#expect -exact "Scripts installer.\r
-#\r
-#Select the options (<SPACE> to select, <UP/DOWN> to choose):\r
-#\r
-#\r
-#\r
-#\r
-#\r
-#^[\[6n"
-#send -- "^[\[59;1R"
-#expect -exact "^[\[?25l^[\[54;1H\[^[\[1;32m*^[\[0m\] ^[\[7mInstall basic dependencies.^[\[27m^[\[55;1H\[^[\[1;32m>
-#send -- " "
-#expect -exact "^[\[54;1H\[ \] ^[\[7mInstall basic dependencies.^[\[27m^[\[55;1H\[^[\[1;32m*^[\[0m\] Install keybo>
-#send -- "^[\[B"
-#expect -exact "^[\[54;1H\[ \] Install basic dependencies.^[\[55;1H\[^[\[1;32m*^[\[0m\] ^[\[7mInstall keyboard sho>
-#send -- " "
-#expect -exact "^[\[54;1H\[ \] Install basic dependencies.^[\[55;1H\[ \] ^[\[7mInstall keyboard shortcuts.^[\[27m^>
-#send -- "^[\[B"
-#expect -exact "^[\[54;1H\[ \] Install basic dependencies.^[\[55;1H\[ \] Install keyboard shortcuts.^[\[56;1H\[^[\>
-#send -- " "
-#expect -exact "^[\[54;1H\[ \] Install basic dependencies.^[\[55;1H\[ \] Install keyboard shortcuts.^[\[56;1H\[ \]>
-#send -- "\r"
-#expect eof' >> /tmp/nautilus-scripts/script.exp
-chmod +x script.exp
-./script.exp
+rm /tmp/nautilus-scripts/install.sh
+cp /ctx/install.sh /tmp/nautilus-scripts/install.sh
+chmod +x install.sh
+bash install.sh
 
 # edit gpu switching config to enable seamless hotplug
 rm /etc/supergfxd.conf
