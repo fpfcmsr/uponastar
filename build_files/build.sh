28557a696c611ba7dc8fd7253d46e8bf10999dde
fpfcmsr <fpf@andrew.cmu.edu>
2025-04-28 19:50:04 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,13 +2,6 @@
 
 set -ouex pipefail
 
-# delete symlinks so packages can be installed to the directories
-#rm /root
-#mkdir /root
-
-rm /opt
-mkdir /opt
-
 ### Install packages
 
 #install packages for dolphin shortcuts + expect for bash noninteractive
@@ -20,11 +13,6 @@ dnf5 -y install btrfs-assistant
 #microsoft fonts install
 dnf5 -y install mscore-fonts-all xorg-x11-font-utils cabextract fontconfig
 rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-
-# install zed editor
-#dnf5 -y config-manager setopt "terra*".enabled=true
-#dnf5 -y install zed
-#dnf5 -y config-manager setopt "terra*".enabled=false
 
 #setup etckeeper
 dnf5 -y install etckeeper 
@@ -49,14 +37,21 @@ mkdir /tmp/rpms
 curl --retry 3 -Lo /tmp/rpms/mfcl2710dwpdrv-4.0.0-1.i386.rpm "https://download.brother.com/welcome/dlf103525/mfcl2710dwpdrv-4.0.0-1.i386.rpm"
 curl --retry 3 -Lo /tmp/rpms/brscan4-0.4.11-1.x86_64.rpm "https://download.brother.com/welcome/dlf105203/brscan4-0.4.11-1.x86_64.rpm"
 curl --retry 3 -Lo /tmp/rpms/brscan-skey-0.3.2-0.x86_64.rpm "https://download.brother.com/welcome/dlf006650/brscan-skey-0.3.2-0.x86_64.rpm"
-curl --retry 3 -Lo /tmp/rpms/brother-udev-rule-type1-1.0.2-0.noarch.rpm "https://download.brother.com/welcome/dlf103900/brother-udev-rule-type1-1.0.2-0.noarch.rpm"
+#curl --retry 3 -Lo /tmp/rpms/brother-udev-rule-type1-1.0.2-0.noarch.rpm "https://download.brother.com/welcome/dlf103900/brother-udev-rule-type1-1.0.2-0.noarch.rpm"
 curl --retry 3 -Lo /tmp/rpms/brmfcfaxdrv-2.0.2-1.x86_64.rpm "https://download.brother.com/welcome/dlf105190/brmfcfaxdrv-2.0.2-1.x86_64.rpm"
 dnf5 install -y /tmp/rpms/*
+#note to self: install the brother udev rules post installation
 
-# move installed packages to the right location
-#mv /root /usr/share/factory
-#ln -s /var/root /root
 
-mv /opt /usr/share/factory
-ln -s /var/opt /opt
+# Things not currently included but may revisit
+# install zed editor
+#dnf5 -y config-manager setopt "terra*".enabled=true
+#dnf5 -y install zed
+#dnf5 -y config-manager setopt "terra*".enabled=false
+
+# for stuff that writes to /opt ??? unsure
+# rm /opt
+# mkdir /opt
+# mv /opt /usr/share/factory
+# ln -s /var/opt /opt
 
