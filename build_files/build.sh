c2e8b66ff547844a4fac028131297da784199c3e
Francesco Fortunelli <frafortunelli@protonmail.com>
2025-04-29 19:17:10 -0400

    update packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,7 @@
 
 set -ouex pipefail
 
-### Install packages
-
+### setup so that /opt is writeable by rpms
 rm /opt
 mkdir /opt
 
@@ -36,22 +35,36 @@ mkdir /tmp/rpms
 curl --retry 3 -Lo /tmp/rpms/mfcl2710dwpdrv-4.0.0-1.i386.rpm "https://download.brother.com/welcome/dlf103525/mfcl2710dwpdrv-4.0.0-1.i386.rpm"
 curl --retry 3 -Lo /tmp/rpms/brscan4-0.4.11-1.x86_64.rpm "https://download.brother.com/welcome/dlf105203/brscan4-0.4.11-1.x86_64.rpm"
 curl --retry 3 -Lo /tmp/rpms/brscan-skey-0.3.2-0.x86_64.rpm "https://download.brother.com/welcome/dlf006650/brscan-skey-0.3.2-0.x86_64.rpm"
-#curl --retry 3 -Lo /tmp/rpms/brother-udev-rule-type1-1.0.2-0.noarch.rpm "https://download.brother.com/welcome/dlf103900/brother-udev-rule-type1-1.0.2-0.noarch.rpm"
+curl --retry 3 -Lo /tmp/rpms/brother-udev-rule-type1-1.0.2-0.noarch.rpm "https://download.brother.com/welcome/dlf103900/brother-udev-rule-type1-1.0.2-0.noarch.rpm"
 curl --retry 3 -Lo /tmp/rpms/brmfcfaxdrv-2.0.2-1.x86_64.rpm "https://download.brother.com/welcome/dlf105190/brmfcfaxdrv-2.0.2-1.x86_64.rpm"
+
+#install bitwarden
+URL=$(curl -s https://api.github.com/repos/bitwarden/clients/releases | jq -r 'first(.[] | .assets[]? | select(.browser_download_url | endswith(".rpm")) | .browser_download_url)')
+
+if [ -n "$URL" ]; then
+    echo "Downloading Bitwarden from $URL"
+    curl -sL -o /tmp/rpms/bitwarden-latest.rpm "$URL"
+else
+    echo "--- Could not find Bitwarden RPM URL"
+    exit 1
+fi
+
+# install all downloaded rpms
 dnf5 install -y /tmp/rpms/*
-#note to self: install the brother udev rules post installation
 
 
-# Things not currently included but may revisit
-# install zed editor
-#dnf5 -y config-manager setopt "terra*".enabled=true
-#dnf5 -y install zed
-#dnf5 -y config-manager setopt "terra*".enabled=false
-
-rm -rf /opt
-mkdir /opt
+# ensure writes to /opt are included in the image and recreate symlink 
+mv /opt /usr/share/factory
 ln -s /var/opt /opt
 
 #microsoft fonts install
 dnf5 -y install mscore-fonts-all xorg-x11-font-utils cabextract fontconfig
 rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+
+
+### Things not currently included but may revisit
+# install zed editor
+#dnf5 -y config-manager setopt "terra*".enabled=true
+#dnf5 -y install zed
+#dnf5 -y config-manager setopt "terra*".enabled=false
