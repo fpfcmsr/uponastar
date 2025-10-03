62847be51f4a0c8345097be5ec4c87dccae2c41f
fpfcmsr <fpf@andrew.cmu.edu>
2025-10-03 18:48:11 +0000

    fix install location

--- build_files/build.sh
+++ build_files/build.sh
@@ -35,7 +35,7 @@ dnf5 -y install steamdeck-kde-presets-desktop
 dnf5 -y copr disable bazzite-org/bazzite
 
 #install specific brother printers
-#mkdir /tmp/rpms
+mkdir /tmp/rpms
 #curl --retry 3 -Lo /tmp/rpms/mfcl2710dwpdrv-4.0.0-1.i386.rpm "https://download.brother.com/welcome/dlf103525/mfcl2710dwpdrv-4.0.0-1.i386.rpm"
 #curl --retry 3 -Lo /tmp/rpms/brscan4-0.4.11-1.x86_64.rpm "https://download.brother.com/welcome/dlf105203/brscan4-0.4.11-1.x86_64.rpm"
 #curl --retry 3 -Lo /tmp/rpms/brscan-skey-0.3.2-0.x86_64.rpm "https://download.brother.com/welcome/dlf006650/brscan-skey-0.3.2-0.x86_64.rpm"
